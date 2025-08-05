5e8b6ad5e17e66ccbe2a8786ffd386970a6de24e
Terry9574 <lilt9574@gmail.com>
2025-08-05 18:38:12 -0500

    Create ImpoundFacility.js

---
+++ src/components/ImpoundFacility.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function ImpoundFacility({ userRole }) {
+  return (
+    <div>
+      <div className="page-header">
+        <h1>ImpoundFacility</h1>
+      </div>
+      
+      <div className="feature-content">
+        <div className="placeholder-message">
+          <i className="fas fa-satellite-dish placeholder-icon"></i>
+          <h2>ImpoundFacility</h2>
+          <p>This feature is coming soon. The Dispatch System will allow you to manage task assignments and track service information.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ImpoundFacility;
