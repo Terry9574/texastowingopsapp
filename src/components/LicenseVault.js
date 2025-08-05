9a0d3b73a0b8b8f06d291186f2578cdc863c4155
Terry9574 <lilt9574@gmail.com>
2025-08-05 18:45:42 -0500

    Create LicenseVault.js

---
+++ src/components/LicenseVault.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function LicenseVault({ userRole }) {
+  return (
+    <div>
+      <div className="page-header">
+        <h1>LicenseVault</h1>
+      </div>
+      
+      <div className="feature-content">
+        <div className="placeholder-message">
+          <i className="fas fa-satellite-dish placeholder-icon"></i>
+          <h2>LicenseVault</h2>
+          <p>This feature is coming soon. The Dispatch System will allow you to manage task assignments and track service information.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LicenseVault;
