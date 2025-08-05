2a97068a875a6871dd57f036ffb6ecf5b0c9376b
Terry9574 <lilt9574@gmail.com>
2025-08-05 16:06:04 -0500

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2025 Texas Towing Ops & Handbook. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
