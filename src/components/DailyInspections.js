9436ae3e16a39cfbee8d5a0942532c3fe92673d0
Terry9574 <lilt9574@gmail.com>
2025-08-05 18:40:59 -0500

    Create DailyInspections.js

---
+++ src/components/DailyInspections.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function DailyInspections({ userRole }) {
+  return (
+    <div>
+      <div className="page-header">
+        <h1>DailyInspections</h1>
+      </div>
+      
+      <div className="feature-content">
+        <div className="placeholder-message">
+          <i className="fas fa-satellite-dish placeholder-icon"></i>
+          <h2>DailyInspections</h2>
+          <p>This feature is coming soon. The Dispatch System will allow you to manage task assignments and track service information.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DailyInspections;
