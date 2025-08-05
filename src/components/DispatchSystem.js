f4a8faee97d4e0cb9228afbf866f50b792c0d6a9
Terry9574 <lilt9574@gmail.com>
2025-08-05 18:33:18 -0500

    Create DispatchSystem.js

---
+++ src/components/DispatchSystem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function DispatchSystem({ userRole }) {
+  return (
+    <div>
+      <div className="page-header">
+        <h1>Dispatch System</h1>
+      </div>
+      
+      <div className="feature-content">
+        <div className="placeholder-message">
+          <i className="fas fa-satellite-dish placeholder-icon"></i>
+          <h2>Dispatch System</h2>
+          <p>This feature is coming soon. The Dispatch System will allow you to manage task assignments and track service information.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DispatchSystem;
