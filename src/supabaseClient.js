852250e801c0bb9ae53b1008c7871f799a1352bf
Terry9574 <lilt9574@gmail.com>
2025-08-06 19:07:09 -0500

    Delete src/supabaseClient.js

--- src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://odxwjyuamvgccpesykts.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9keHdqeXVhbXZnY2NwZXN5a3RzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ0Mjk4MTQsImV4cCI6MjA3MDAwNTgxNH0.BnzEn6Ep40ysR0hRcuYeY61Rlxohd2YDIvuHbQvDeAc';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
