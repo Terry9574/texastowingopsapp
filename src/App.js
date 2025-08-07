5ac2b8106bf63b55f0f996f35251b0bb5806beef
Terry9574 <lilt9574@gmail.com>
2025-08-06 19:21:43 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{padding: '20px', textAlign: 'center'}}>
+      <h1>Texas Towing Ops</h1>
+      <p>Basic app version</p>
+    </div>
+  );
+}
+
+export default App;
