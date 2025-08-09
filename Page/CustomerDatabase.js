c473ae6610fa0b99314376fba96dd42b3394a4ae
Terry9574 <lilt9574@gmail.com>
2025-08-08 19:04:46 -0500

    Update CustomerDatabase.js

--- Page/CustomerDatabase.js
+++ Page/CustomerDatabase.js
@@ -393,7 +393,6 @@ function CustomerDatabase() {
         )}
       </div>
       
-      {/* Customer Details Modal */}
       {selectedCustomer && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4 z-50">
           <div className="bg-white rounded-lg shadow-xl max-w-2xl w-full max-h-[90vh] overflow-y-auto">
